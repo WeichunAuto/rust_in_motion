536304da1212c106eab0636a58a5d6d35edc39bd
WeichunAuto <seven.wangweichun@outlook.com>
2026-02-05 10:18:58 +1300

    integrated with tailwindcss

--- src/components/homepage.rs
+++ src/components/homepage.rs
@@ -11,38 +11,40 @@ pub fn HomePage() -> impl IntoView {
     let menu_once = OnceResource::new(get_menus());
 
     view! {
-        <div class="w-full h-40 bg-amber-300 flex gap-4">
-            <Suspense fallback=|| view! { <div>"菜单加载中..."</div> }>
-            {
-                move || {
-                    menu_once.get().map(|result| {
-                        match result {
-                            Ok(menus) => Either::Left(view! {
-                                <ForEnumerate
-                                    each = move || menus.clone()
-                                    key = |menu| menu.id
-                                    children = move |_, menu| {
-                                        let href_link = if menu.menu_name == "About Me" {
-                                            "/About_Me".to_string()
-                                        } else {
-                                            format!("/{}", menu.menu_name)
-                                        };
-                                        view! {
-                                            <div>
-                                                <A href=href_link>{menu.menu_name}</A>
-                                            </div>
+        <div class="w-full h-screen bg-amber-300 flex gap-4 ">
+            <div class="w-full h-28 bg-[url('/images/header_bg.png')]">
+                <Suspense fallback=|| view! { <div>"菜单加载中..."</div> }>
+                {
+                    move || {
+                        menu_once.get().map(|result| {
+                            match result {
+                                Ok(menus) => Either::Left(view! {
+                                    <ForEnumerate
+                                        each = move || menus.clone()
+                                        key = |menu| menu.id
+                                        children = move |_, menu| {
+                                            let href_link = if menu.menu_name == "About Me" {
+                                                "/About_Me".to_string()
+                                            } else {
+                                                format!("/{}", menu.menu_name)
+                                            };
+                                            view! {
+                                                <div>
+                                                    <A href=href_link>{menu.menu_name}</A>
+                                                </div>
+                                            }
                                         }
-                                    }
-                                />
-                            }),
-                            Err(_) => Either::Right(view! {
-                                <div>"加载菜单错误！"</div>
+                                    />
+                                }),
+                                Err(_) => Either::Right(view! {
+                                    <div>"加载菜单错误！"</div>
+                            })
+                            }
                         })
-                        }
-                    })
+                    }
                 }
-            }
-            </Suspense>
+                </Suspense>
+            </div>
 
             <Outlet/>
         </div>
